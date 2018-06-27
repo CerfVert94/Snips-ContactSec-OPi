0f0c158b6b3379e1ca14fae3c56887c52db541b1
cerfvert94 <roqyun.ko@etc.upmc.fr>
2018-06-27 15:19:38 +0200

    Open fnc test 6

--- action-contact.sec.py
+++ action-contact.sec.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
+import os
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -10,21 +11,18 @@ def intent_received(hermes, intent_message):
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name	
 
-	try :	
-		gpio_pin = open("/home/pi/log.txt", "w")
-		if intentName == 'Roqyun:Allumage' :
-			if probability > 0.9 :
-				sentence = "J allume la lumiere"
-			else :
-				sentence = " Je n'ai pas compris"
-		elif 	intentName == 'Roqyun:Extinction' :
-			if probability > 0.9 :
-				sentence = "Je eteins la lumiere"
-			else :
-				sentence = " Je n'ai pas compris"
-		gpio_pin.close()
-	except IOError:
-		sentence = "Erreur"
+	gpio_pin = os.open("/home/pi/log.txt",os.O_WRONLY)
+	if intentName == 'Roqyun:Allumage' :
+		if probability > 0.9 :
+			sentence = "J allume la lumiere"
+		else :
+			sentence = " Je n'ai pas compris"
+	elif 	intentName == 'Roqyun:Extinction' :
+		if probability > 0.9 :
+			sentence = "Je eteins la lumiere"
+		else :
+			sentence = " Je n'ai pas compris"
+	os.close(gpio_pin)
 			
 
 	hermes.publish_end_session(intent_message.session_id, sentence)
