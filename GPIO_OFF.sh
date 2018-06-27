b8ee5b4f41b7fe672049af2b6d8936d3d76e6431
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-27 17:43:14 +0200

    New GPIO Test 8

---
+++ GPIO_OFF.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Hello world"
+echo '12' | sudo tee /sys/class/gpio/export
+echo 'out' | sudo tee /sys/class/gpio/gpio12/directoin
+echo '0' | sudo tee /sys/class/gpio/gpio12/value
+echo '12' | sudo tee /sys/class/gpio/unexport
