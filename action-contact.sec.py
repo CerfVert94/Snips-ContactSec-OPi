40ef4f34357cc72740dea3e8891a7cfd6b8f3e41
cerfvert94 <lmk@DESKTOP-UUJBKND.localdomain>
2018-06-27 12:49:25 +0200

    gpioTEST14

--- action-contact.sec.py
+++ action-contact.sec.py
@@ -6,27 +6,29 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-
 def intent_received(hermes, intent_message):
 	GPIO.setwarnings(False);
-	GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BOARD)
+	
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name	
+
 	
 	if intentName == 'Roqyun:Allumage' :
 		if probability > 0.9 :
-			sentence = "Je allume la lumiere"
+			sentence = "J allume la lumiere"
+			GPIO.setup(12, GPIO.OUT, initial=GPIO.HIGH)
 		else :
 			sentence = " Je n'ai pas compris"
 	elif intentName == 'Roqyun:Extinction' :
 		if probability > 0.9 :
 			sentence = "Je eteins la lumiere"
+			GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
 		else :
-			sentence = " Je n'ai pas compris"	
+			sentence = " Je n'ai pas compris"
+			
 			
 
 	hermes.publish_end_session(intent_message.session_id, sentence)
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
-
-	
