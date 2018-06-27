189385dba09f4591c656b7f3d795ccb62a21c1b1
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-27 15:48:36 +0200

    New GPIO Test 1

--- action-contact.sec.py
+++ action-contact.sec.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
-import os
+from pyA20.gpio import gpio
+from pyA20.gpio import port
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -10,18 +11,26 @@ def intent_received(hermes, intent_message):
 
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name	
+	
+	gpio.init()
+	gpio.setcfg(port.PA12, gpio.OUTPUT)
 
+	
 	gpio_pin = os.open("/home/pi/log.txt",os.O_WRONLY)
 	if intentName == 'Roqyun:Allumage' :
 		if probability > 0.9 :
+			gpio.output(port.PA12, gpio.HIGH)
 			sentence = "J allume la lumiere"
 		else :
 			sentence = " Je n'ai pas compris"
 	elif 	intentName == 'Roqyun:Extinction' :
 		if probability > 0.9 :
+			gpio.output(port.PA12, gpio.LOW)
 			sentence = "Je eteins la lumiere"
 		else :
-			sentence = " Je n'ai pas compris"
+			sentence = " Je n'ai pas compris"		
+	else :
+		sentence = " Je n'ai pas compris"
 			
 
 	hermes.publish_end_session(intent_message.session_id, sentence)
