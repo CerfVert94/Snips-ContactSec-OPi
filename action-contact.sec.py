7c76ddb98ab063daa2edeb8b64954049ab6bd4a9
cerfvert94 <lmk@DESKTOP-UUJBKND.localdomain>
2018-06-27 12:39:27 +0200

    gpioTEST11

--- action-contact.sec.py
+++ action-contact.sec.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
+import RPi.GPIO as GPIO
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -7,6 +8,9 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 def intent_received(hermes, intent_message):
+	GPIO.setwarnings(False);
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name	
 	gpio = open("/home/pi/test.py","w")
