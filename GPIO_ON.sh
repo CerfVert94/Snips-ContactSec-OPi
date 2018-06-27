33241965b8ba4de839ffe2530a54a22f81050e73
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-27 17:50:04 +0200

    Shell scripts files corrected

--- GPIO_ON.sh
+++ GPIO_ON.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "Hello world"
 echo '12' | sudo tee /sys/class/gpio/export
-echo 'out' | sudo tee /sys/class/gpio/gpio12/directoin
+echo 'out' | sudo tee /sys/class/gpio/gpio12/direction
 echo '1' | sudo tee /sys/class/gpio/gpio12/value
 echo '12' | sudo tee /sys/class/gpio/unexport
