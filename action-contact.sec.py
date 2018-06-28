3c45ee91ad15cdd7272ffed0585bccbb1a87d910
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-28 11:35:52 +0200

    test

--- action-contact.sec.py
+++ action-contact.sec.py
@@ -12,8 +12,11 @@ def intent_received(hermes, intent_message):
 	intentName = intent_message.intent.intent_name	
 	
 	command_line = "/var/lib/snips/skills/Snips-ContactSec-OPi/GPIO_ON.sh"
-	subprocess.Popen(command_line)
-	subprocess.call(['echo', '123', '>', 'log.txt'])
+	try :
+		subprocess.Popen(command_line)
+	except :
+		hermes.publish_end_session(intent_message.session_id, "Je suis un teste")
+		return
 	if intentName == 'Roqyun:Allumage' :
 		if probability > 0.9 :
 			sentence = "J allume la lumiere"
