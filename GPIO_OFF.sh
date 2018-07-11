9e26a416f3e5e1765e9d408f931a323f482a6dc6
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-07-11 11:28:21 +0200

    delete all

--- GPIO_OFF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo '12' | sudo tee /sys/class/gpio/export
-echo 'out' | sudo tee /sys/class/gpio/gpio12/direction
-echo '0' | sudo tee /sys/class/gpio/gpio12/value
-echo '12' | sudo tee /sys/class/gpio/unexport
